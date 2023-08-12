9bd9a4725ba382c2c32b85e4ce74dbe5e84348be
SantiZai <santyagozaidan@gmail.com>
2023-08-12 16:06:12 -0300

    cors added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { CourtsController } from './courts/courts.controller';
-import { PlayersController } from './players/players.controller';
-import { ReservationsController } from './reservations/reservations.controller';
-import { UsersService } from './users/users.service';
+import { CourtsController } from "./courts/courts.controller";
+import { PlayersController } from "./players/players.controller";
+import { ReservationsController } from "./reservations/reservations.controller";
+import { UsersService } from "./users/users.service";
 
 @Module({
 	imports: [],
-	controllers: [AppController, CourtsController, PlayersController, ReservationsController],
+	controllers: [
+		AppController,
+		CourtsController,
+		PlayersController,
+		ReservationsController,
+	],
 	providers: [AppService, UsersService],
 })
 export class AppModule {}
