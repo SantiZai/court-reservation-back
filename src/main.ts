26bdf3c376de1ac0bab3ebbe893a55441ccf702b
SantiZai <santyagozaidan@gmail.com>
2023-10-10 13:26:55 -0300

    new port

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	app.use(cors());
 	app.use(cookieParser());
-	await app.listen(8080);
+	await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
